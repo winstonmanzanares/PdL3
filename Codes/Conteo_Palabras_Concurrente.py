9c160f2d87d5a87148a8280ec6e159163a64196c
Mateo Sánchez <mateo.sanchez@estudiantes.utec.edu.uy>
2025-10-26 14:05:38 -0300

    Adición procConcTime():
    Permite no usar el logger y retorna valor en segundos de procesamiento

--- Codes/Conteo_Palabras_Concurrente.py
+++ Codes/Conteo_Palabras_Concurrente.py
@@ -17,6 +17,50 @@ def count_words(filename, result_queue):
     result_queue.put((os.path.basename(filename), len(palabras), fin - inicio))
     logger.debug(f"{os.path.basename(filename)}: count_words demoró {fin - inicio:.5f}s")
 
+
+
+########################################################################
+# Bloque de funciones similares a las originales pero sin el datalogger
+
+def silcount_words(filename, result_queue):
+    """Función ejecutada por cada proceso para contar palabras."""
+    inicio = time.time()
+    with open(filename, 'r', encoding='utf-8') as archivo:
+        contenido = archivo.read()
+    palabras = contenido.split()
+    fin = time.time()
+    result_queue.put((os.path.basename(filename), len(palabras), fin - inicio))
+
+def procConcTime():
+    directorio = os.getcwd()
+    archivos = [os.path.join(directorio, f) for f in os.listdir(directorio) if f.endswith('.txt') and 'ecuaciones' not in f]
+
+    cola = mp.Queue()
+    procesos = []
+
+    inicio_total = time.time()
+    for archivo in archivos:
+        proceso = mp.Process(target=silcount_words, args=(archivo, cola))
+        procesos.append(proceso)
+        proceso.start()
+
+    for proceso in procesos:
+        proceso.join()
+    fin_total = time.time()
+    total_palabras = 0
+    while not cola.empty():
+        nombre_archivo, cantidad, tiempo_proc = cola.get()
+        total_palabras += cantidad
+
+    return fin_total - inicio_total 
+
+
+########################################################################
+
+
+
+
+
 def procesar_concurrente():
     """Procesa los archivos usando varios procesos concurrentes."""
     directorio = os.getcwd()
