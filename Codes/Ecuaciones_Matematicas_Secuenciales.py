bc0ee22830b78eade60dac55214d84305f46fd5b
Mateo Sánchez <mateo.sanchez@estudiantes.utec.edu.uy>
2025-10-26 15:29:51 -0300

    Adición de procEcSecTime():
    No usa logger, retorna tiempo de procesamiento

--- Codes/Ecuaciones_Matematicas_Secuenciales.py
+++ Codes/Ecuaciones_Matematicas_Secuenciales.py
@@ -14,6 +14,44 @@ def solve_equation(equation):
     logger.debug(f"Integral {equation} calculada en {fin - inicio:.5f}s")
     return (equation, resultado_integral, fin - inicio)
 
+
+
+
+########################################################################
+# Bloque de funciones similares a las originales pero sin el datalogger
+
+
+def silsolve_equation(equation):
+    """Resuelve una integral simbólica de forma secuencial."""
+    x, a, b = symbols('x a b')
+    inicio = time.time()
+    equation = equation.replace('^', '**')
+    resultado_integral = integrate(eval(equation), x)
+    fin = time.time()
+    return (equation, resultado_integral, fin - inicio)
+
+
+def procEcSecTime():
+    with open("ecuaciones.txt", "r", encoding="utf-8") as f:
+        ecuaciones = [line.strip() for line in f.readlines()]
+
+    inicio_total = time.time()
+
+    for eq in ecuaciones:
+        ecuacion, resultado, tiempo = silsolve_equation(eq)
+
+    fin_total = time.time()
+    return fin_total - inicio_total
+
+
+
+########################################################################
+
+
+
+
+
+
 def procesar_ecuaciones():
     """Lee ecuaciones.txt y las integra una por una."""
     with open("ecuaciones.txt", "r", encoding="utf-8") as f:
