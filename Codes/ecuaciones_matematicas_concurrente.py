205850b3847071df76803dc9580e78a58c79e7e3
Mateo Sánchez <mateo.sanchez@estudiantes.utec.edu.uy>
2025-10-26 14:07:27 -0300

    Adición de procEcConTime():
    No usa el logger, retorna tiempo de procesamiento en segundos

--- Codes/ecuaciones_matematicas_concurrente.py
+++ Codes/ecuaciones_matematicas_concurrente.py
@@ -29,6 +29,62 @@ def thread_solve_equations(equations, result_pipe):
     for thread in threads:
         thread.join()
 
+
+
+
+
+
+########################################################################
+# Bloque de funciones similares a las originales pero sin el datalogger
+
+def silsolve_equation(equation, result_pipe):
+    """Resuelve una integral simbólica en un hilo y envía el resultado por una tubería."""
+    x, a, b = symbols('x a b')
+    try:
+        inicio = time.time()
+        equation = equation.replace('^', '**')
+        resultado = integrate(eval(equation), x)
+        fin = time.time()
+        tiempo = fin - inicio
+        result_pipe.send((equation, resultado, tiempo))
+        #logger.debug(f"Integral {equation} calculada en {tiempo:.5f}s")
+    except Exception as e:
+        result_pipe.send((equation, f"Error: {str(e)}", 0))
+
+def silthread_solve_equations(equations, result_pipe):
+    """Crea un hilo por cada ecuación y espera a que terminen."""
+    threads = []
+    for ecuacion in equations:
+        thread = th.Thread(target=silsolve_equation, args=(ecuacion, result_pipe))
+        threads.append(thread)
+        thread.start()
+    for thread in threads:
+        thread.join()
+
+def procEcConTime():
+    """Ejecuta las ecuaciones del archivo en paralelo con hilos."""
+    with open("ecuaciones.txt", "r", encoding="utf-8") as f:
+        ecuaciones = [line.strip() for line in f.readlines()]
+
+    tuberia1, tuberia2 = mp.Pipe()
+    inicio_total = time.time()
+    silthread_solve_equations(ecuaciones, tuberia2)
+    while tuberia1.poll():
+        ecuacion, integral, tiempo = tuberia1.recv()
+     
+    fin_total = time.time()
+    return fin_total - inicio_total
+
+
+
+########################################################################
+
+
+
+
+
+
+
 def procesar_ecuaciones_concurrente():
     """Ejecuta las ecuaciones del archivo en paralelo con hilos."""
     with open("ecuaciones.txt", "r", encoding="utf-8") as f:
